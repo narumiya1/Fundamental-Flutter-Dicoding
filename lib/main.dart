8918c6e61de8e74384af7799b95c117d1728be37
narumiya1 <narumiiyamei@gmail.com>
2022-12-22 21:28:13 +0700

    mainn

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:restaurant_submission1/provider/search.dart';
 import 'package:restaurant_submission1/styles.dart';
 import 'package:restaurant_submission1/ui/detail_page.dart';
 import 'package:restaurant_submission1/ui/search_page.dart';
+import 'package:restaurant_submission1/utils/navigation.dart';
 
 import 'home_page.dart';
 
@@ -36,29 +37,16 @@ class MyApp extends StatelessWidget {
      child: MaterialApp(
       title: 'Restaurant Apps',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primaryColor: primaryColor,
         scaffoldBackgroundColor: primaryColor,
         textTheme: textTheme,
       ),
-      initialRoute: HomePage.routeName,
+       initialRoute: HomePage.routeName,
       routes: {
-        HomePage.routeName: (context) => const HomePage(),
-        // RestaurantDetailPage.routeName: (context) => RestaurantDetailPage(
-        //       restaurant:
-        //           ModalRoute.of(context)?.settings.arguments as Restaurant,
-        //     ),
+        HomePage.routeName: (context) =>  HomePage(),
         DetailPage.routeName: (context) => DetailPage(
-              restaurant:
-                  ModalRoute.of(context)?.settings.arguments as Restaurant,
+                id: ModalRoute.of(context)?.settings.arguments as String,
             ),
         Search.routeName: (context) => const Search(),
       },
