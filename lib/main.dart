8b711cada26b65a8b3014429d0a5e92663ace443
narumiya1 <narumiiyamei@gmail.com>
2022-12-29 16:02:50 +0700

    main.dart : - ChangeNotifierProvider : BottomNavigationBarLst, DatabaseProovider, ScheduleProvider, SharedPreferenceProviders. - MainPage.routeName,

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_submission1/api_data/api_serv.dart';
-import 'package:restaurant_submission1/model/list_model.dart';
+import 'package:restaurant_submission1/data/database_helper.dart';
+import 'package:restaurant_submission1/provider/bottom_navigation_bar.dart';
+import 'package:restaurant_submission1/provider/database_provider.dart';
 import 'package:restaurant_submission1/provider/restaurant_provider.dart';
+import 'package:restaurant_submission1/provider/schedule_provider.dart';
 import 'package:restaurant_submission1/provider/search.dart';
+import 'package:restaurant_submission1/provider/shared_preferances_provider.dart';
 
 import 'package:restaurant_submission1/styles.dart';
 import 'package:restaurant_submission1/ui/detail_page.dart';
+import 'package:restaurant_submission1/ui/main_page.dart';
 import 'package:restaurant_submission1/ui/search_page.dart';
-import 'package:restaurant_submission1/utils/navigation.dart';
 
-import 'home_page.dart';
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+FlutterLocalNotificationsPlugin();
 void main() {
   runApp(const MyApp());
 }
@@ -22,35 +28,41 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
     return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) =>
-              RestaurantProvider(apiService: ApiServ()),
-        ),
-        ChangeNotifierProvider(
-          create: (context) =>
-              SearchProvider(restaurantApi: ApiServ()),
-        ),
-      ],
-     child: MaterialApp(
-      title: 'Restaurant Apps',
-      theme: ThemeData(
-
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: primaryColor,
-        textTheme: textTheme,
-      ),
-       initialRoute: HomePage.routeName,
-      routes: {
-        HomePage.routeName: (context) =>  HomePage(),
-        DetailPage.routeName: (context) => DetailPage(
-                id: ModalRoute.of(context)?.settings.arguments as String,
-            ),
-        Search.routeName: (context) => const Search(),
-      },
-     )
-    );
+        providers: [
+          ChangeNotifierProvider(
+              create: (context)=>BottomNavigationBarLst(),),
+          ChangeNotifierProvider(
+              create: (context) =>
+                  DatabaseProovider(databaseHelpers: DatabaseHelper())),
+          ChangeNotifierProvider(
+            create: (context) => RestaurantProvider(apiService: ApiServ()),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SearchProvider(restaurantApi: ApiServ()),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => ScheduleProvider(),
+          ),
+          ChangeNotifierProvider(
+            create: (context) => SharedPreferenceProviders(),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Restaurant Apps',
+          theme: ThemeData(
+            primaryColor: primaryColor,
+            scaffoldBackgroundColor: primaryColor,
+            textTheme: textTheme,
+          ),
+          initialRoute: MainPage.routeName,
+          routes: {
+            MainPage.routeName: (context) => MainPage(),
+            DetailPage.routeName: (context) => DetailPage(
+                  id: ModalRoute.of(context)?.settings.arguments as String,
+                ),
+            Search.routeName: (context) => const Search(),
+          },
+        ));
   }
 }
